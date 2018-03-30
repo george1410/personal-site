3fd8147074ad513a052882d60610c15c6d3788eb
George McCarron <hello@georgemccarron.com>
2018-03-30 23:35:54 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>George McCarron</title>
+    <link href="https://fonts.googleapis.com/css?family=Cardo:400,400i,700|Montserrat:400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col col-md-9">
+                    <h1>George McCarron</h1>
+                    <p class="intro">
+                        Hello! Born and raised in the sunny South East of England, I'm now a first year Computer Science student at the University of Nottingham getting into just over £40,000 of debt.
+                    </p>
+                    <a class="btn btn-outline-dark intro-btn" href="mailto:hello@georgemccarron.com">Email me</a>
+                    <div class="social-area">
+                        <a href="https://twitter.com/george_mccarron"><i class="fab fa-twitter"></i></a>
+                        <a href="https://instagram.com/george_mccarron"><i class="fab fa-instagram"></i></a>
+                        <a href="https://snapchat.com/add/georgemccarron"><i class="fab fa-snapchat-ghost"></i></a>
+                        <a href="https://open.spotify.com/user/georgieboy14"><i class="fab fa-spotify"></i></a>
+                        <a href="https://github.com/george1410"><i class="fab fa-github"></i></a>
+                        <a href="https://linkedin.com/in/george-mccarron-08517690"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="fill"></div>
+</body>
+</html>
