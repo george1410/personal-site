f96e99a0aed12f8ef81e04720ea11e4a8a1f21db
George McCarron <hello@georgemccarron.com>
2019-02-08 20:53:58 +0000

    removed deployment ip control

--- deploy.php
+++ deploy.php
@@ -47,12 +47,6 @@ foreach ($allowed_ips as $allow) {
     }
 }
 
-if (!$allowed) {
-	header('HTTP/1.1 403 Forbidden');
- 	echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
-    echo "</pre>\n</body>\n</html>";
-    exit;
-}
 
 flush();
 
