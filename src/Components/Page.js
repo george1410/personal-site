6fa91317657913eb22599f47c9637a53292bd00f
George McCarron <hello@georgemccarron.com>
2019-02-10 13:26:30 +0000

    set up basics

---
+++ src/Components/Page.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from './Header';
+
+class Page extends Component {
+    render() {
+        return (
+            <Header></Header>
+        )
+    }
+}
+
+export default Page;
