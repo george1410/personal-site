cb60aa36ba52f01e164cd1dae4a6518d08e4ea32
George McCarron <hello@georgemccarron.com>
2019-06-24 18:33:36 +0100

    fresh start

--- server/server.js
+++ server/server.js
@@ -4,15 +4,15 @@
  * Module dependencies.
  */
 
-var app = require('../app');
-var debug = require('debug')('server:server');
+var app = require('./app');
+var debug = require('debug')('express-react:server');
 var http = require('http');
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
+var port = normalizePort(process.env.PORT || '3001');
 app.set('port', port);
 
 /**
--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Page from './Components/Page';
-import * as serviceWorker from './serviceWorker';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faChevronCircleDown } from '@fortawesome/free-solid-svg-icons';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-library.add(fab, faChevronCircleDown);
-ReactDOM.render(<Page />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
