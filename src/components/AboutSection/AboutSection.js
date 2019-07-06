e6a3b60b33bbd90ee1a084248c8c2b99a41b91b0
George McCarron <hello@georgemccarron.com>
2019-07-07 00:07:47 +0100

    finished the social area

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -9,14 +9,14 @@ function AboutSection() {
     <TitledArea title="Intro">
       <p>
         My name is George McCarron and I'm currently studying Computer Science at the University 
-        of Nottingham. I have a particular interest in building high quality, reliable &amp;
-        scalable web applications using modern technologies.
+        of Nottingham. I have a particular interest in building high quality, reliable, and
+        scalable web applications using modern technologies. I'm also a big fan of Oxford Commas...
       </p>
     </TitledArea>
     <TitledArea title="Skills">
       <SkillsArea />
     </TitledArea>
-    <TitledArea title="Skills">
+    <TitledArea title="Social">
       <SocialArea />
     </TitledArea>
     </div>
